5381adf1e163e32db6cd4b774a78bb62cbb409f3
pedro@gmail.com <pedro@gmail.com>
2023-11-22 10:33:48 -0300

    Teste

---
+++ projeto/resources/views/_layout/principal.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagtina @yield('titulo')</title>
+</head>
+<body>
+    <div style="background-color: red; width: 200px; height: 100px">
+        @yield('conteudo')
+    </div>
+</body>
+</html>
