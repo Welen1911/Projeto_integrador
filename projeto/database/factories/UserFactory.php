5c61cab158f69b0ce9c2c8c85bc6acd9c444d517
Welen1911 <welenalmeida2210@gmail.com>
2023-11-26 10:37:16 -0300

    Refactor: factory user

--- projeto/database/factories/UserFactory.php
+++ projeto/database/factories/UserFactory.php
@@ -14,12 +14,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $tipoConta = ['candidato', 'empresa'];
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'tipo_conta' => $tipoConta[rand(0, 1)],
         ];
     }
 
