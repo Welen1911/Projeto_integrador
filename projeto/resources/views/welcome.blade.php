d52fdc19418c31277eb8ced71a6b8db8a33d6dc0
root <root@Nitro5>
2023-11-26 23:28:40 -0300

    frontend

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -334,55 +334,123 @@
 					Veja todas as vagas!
 				</p>
 			</a>
+
+	<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+      <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg>
+        <span class="ml-3 text-xl">Footer</span>
+      </a>
+      <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+    </div>
+    <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">INSTITUCIONAL</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Sobre nós</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Política de Privacidade</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Central de Ajuda</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Termos</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Paceiros</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Trabalhe Conosco</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CANDIDATO</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Ajuda</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Blog</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Vagas</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Guia de Cargos</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Aviso Legal</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Busca de Emprego Segura</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">EMPRESA</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Ajuda</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Blog</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Anunciar</a>
+          </li>
+          <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Buscar candidatos</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Produtos Para Empresa</a>
+          </li>
+		  <li>
+            <a href="http://www.youtube.com" a class="text-gray-600 hover:text-gray-800">Busca Certa</a>
+          </li>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gray-100">
+    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p class="text-gray-500 text-sm text-center sm:text-left">© 2023 - Empresa
+      </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+        <a class="text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </div>
+</footer>
 		</div>
     </div>
-
-
-	<div class="inset-x-0 mt-20 w-full lg:h-4/5 z-0 px-8 py-1 h-auto bg-gray-100 sm:px-16 sm:h-4/5 lg:px-40">
-		<!-- Informação do lado Esquedo do Banner_-->
-        <div class="flex inset-x-0 flex-col justify-between p-3 mt-4  xl:flex-row lg:content-center">
-			<div class="grid  content-center">
-				<!-- Titulo e subtitulo -->
-				<h2 class="text-4xl font-extrabold text-indigo-800">
-					Explore o site de vagas de emprego mais completo do Rio Grande do Norte!
-				</h2>
-				<p class="my-4 text-lg text-indigo-600">
-					Faça sua pesquisa e veja as melhores opções de vagas do site de empregos que é o preferido doskdmrfpofor recrutadores.				
-				</p>
-			
-				<!-- Barras de Pesquisa -->
-				<form class="mt-4 flex flex-col flex-nowrap gap-5 xl:flex-row">   
-					<div class="relative f">
-						<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-							<svg class="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-							</svg>
-						</div>
-						<input type="search" name="barPesquisarVagas" id="default-search" class=" w-80 sm:w-full lg:w-full p-4 ps-10 text-sm rounded-lg text-gray-900 dark:placeholder-gray-400 border border-blue-500" placeholder="Procure as vagas" required>
-					</div>
-					<div class="relative ">
-						<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-400">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-							</svg>
-						</div>
-						<input type="search" name="barPesquisarLocal" id="default-search" class=" w-80 sm:w-full lg:w-full p-4 ps-10 text-sm rounded-lg text-gray-900 dark:placeholder-gray-400 border border-blue-500" placeholder="Local da vaga" required>
-					</div>
-					<button type="submit" class="block p-2 text-sm font-medium text-white bg-indigo-800 rounded-lg border border-indigo-800 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-indigo-300 ">
-						Buscar vagas
-					</button>
-				</form>
-			</div>
-			
-		
-			<div class=" sm:ml-auto lg:ml-auto ">
-		
-				<img class="" src="{{ asset('imagens/ImgBanner.png') }}" alt="ImgBanner">
-
-			</div>
-		
-		</div>
     </div>
 </header> 
 </body>
