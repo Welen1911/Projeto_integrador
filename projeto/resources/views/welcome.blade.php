3200bf4ae3d541a917b8240c16c0e90429cb5356
pedro@gmail.com <pedro@gmail.com>
2023-11-26 22:52:38 -0300

    Upgrade

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -336,6 +336,54 @@
 			</a>
 		</div>
     </div>
+
+
+	<div class="inset-x-0 mt-20 w-full lg:h-4/5 z-0 px-8 py-1 h-auto bg-gray-100 sm:px-16 sm:h-4/5 lg:px-40">
+		<!-- Informação do lado Esquedo do Banner_-->
+        <div class="flex inset-x-0 flex-col justify-between p-3 mt-4  xl:flex-row lg:content-center">
+			<div class="grid  content-center">
+				<!-- Titulo e subtitulo -->
+				<h2 class="text-4xl font-extrabold text-indigo-800">
+					Explore o site de vagas de emprego mais completo do Rio Grande do Norte!
+				</h2>
+				<p class="my-4 text-lg text-indigo-600">
+					Faça sua pesquisa e veja as melhores opções de vagas do site de empregos que é o preferido doskdmrfpofor recrutadores.				
+				</p>
+			
+				<!-- Barras de Pesquisa -->
+				<form class="mt-4 flex flex-col flex-nowrap gap-5 xl:flex-row">   
+					<div class="relative f">
+						<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+							<svg class="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+							</svg>
+						</div>
+						<input type="search" name="barPesquisarVagas" id="default-search" class=" w-80 sm:w-full lg:w-full p-4 ps-10 text-sm rounded-lg text-gray-900 dark:placeholder-gray-400 border border-blue-500" placeholder="Procure as vagas" required>
+					</div>
+					<div class="relative ">
+						<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-400">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+							</svg>
+						</div>
+						<input type="search" name="barPesquisarLocal" id="default-search" class=" w-80 sm:w-full lg:w-full p-4 ps-10 text-sm rounded-lg text-gray-900 dark:placeholder-gray-400 border border-blue-500" placeholder="Local da vaga" required>
+					</div>
+					<button type="submit" class="block p-2 text-sm font-medium text-white bg-indigo-800 rounded-lg border border-indigo-800 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-indigo-300 ">
+						Buscar vagas
+					</button>
+				</form>
+			</div>
+			
+		
+			<div class=" sm:ml-auto lg:ml-auto ">
+		
+				<img class="" src="{{ asset('imagens/ImgBanner.png') }}" alt="ImgBanner">
+
+			</div>
+		
+		</div>
+    </div>
 </header> 
 </body>
 </html>
