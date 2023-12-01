5b3c6deb71b0032aea0d964683cfd92401eaac84
freitas777 <lucasfdossantos17@gmail.com>
2023-12-01 19:05:52 -0300

    commit

--- projeto/resources/views/vaga.blade.php
+++ projeto/resources/views/vaga.blade.php
@@ -58,7 +58,45 @@
         
     </div>-->
 
-    
+
+    <div class="inset-x-0 w-full lg:h-4/5 z-0 px-8 py-1 h-auto sm:px-16 sm:h-4/5 lg:px-40">
+        <div class="flex inset-x-0 flex-col justify-between p-3 mt-4 xl:flex-row lg:content-center">
+            <div class="grid content-center">
+                <h1 class="text-4xl text-indigo-800 flex-auto">Sobre a vaga</h1>
+                <p class="mt-3 text-justify ml-7">Para atendermos nossos clientes de forma ágil, assertiva e humana e continuar contribuindo para o crescimento da cooperativa, desejamos candidatos que se identifiquem com nossos valores e queiram parte do desse propósito.
+                    Nossa visão é nos tornarmos referência nacional em gestão de saúde, com inovação, sustentabilidade e liderança de mercado.
+                    E para isso, precisamos de pessoas que nos apoiem, levando essa visão adiante em suas atividades.
+                    Já pensou em fazer arte de uma equipe centrada no cuidado, que busca ser a diferença na vida das pessoas?
+                    E melhor ainda, estar dentro do maior sistema de cooperativas de saúde do mundo?</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="inset-x-0 w-full lg:h-4/5 z-0 px-8 py-1 h-auto sm:px-16 sm:h-4/5 lg:px-40">
+        <div class="flex inset-x-0 flex-col justify-between p-3 mt-4 xl:flex-row lg:content-center">
+            <div class="grid content-center">
+                <h1 class="text-3xl text-indigo-800 flex-auto">Responsabilidades e atribuições:</h1>
+                    <ul class="list-disc pl-4 mt-3 ml-7">
+                    <li class="mb-2">Apoiar no desenvolvimento de sistemas e aplicações low code e nos ajustes em banco de dados;</li>
+                    <li class="mb-2">Realizar manutenção de sistemas e aplicações utilizando metodologias ágeis;</li>
+                    <li class="mb-2">Manter registros atualizados na aplicação de chamados, tarefas e ações realizadas.</li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="inset-x-0 w-full lg:h-4/5 z-0 px-8 py-1 h-auto sm:px-16 sm:h-4/5 lg:px-40">
+        <div class="flex inset-x-0 flex-col justify-between p-3 mt-4 xl:flex-row lg:content-center">
+            <div class="grid content-center">
+                <h1 class="text-3xl text-indigo-800 flex-auto">Requisitos e qualificações</h1>
+                    <ul class="list-disc pl-4 mt-3 ml-7">
+                    <li class="mb-2">Curso técnico em informática concluído ou cursando superior nas áreas de Análise e Desenvolvimento de Sistemas,
+                         Ciência da Computação, Gestão da Tecnologia da Informação ou Sistemas de Informação;</li>
+                    <li class="mb-2">Desejável conhecimento em desenvolvimento low code e/ou aplicação Mendix.</li>
+                    </ul>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
