a996462bcfbbdb2039d8fd531ffc78db3ab46601
Welen1911 <welenalmeida2210@gmail.com>
2023-12-02 18:59:54 -0300

    Feat: header links implements

--- projeto/resources/views/components/app/header.blade.php
+++ projeto/resources/views/components/app/header.blade.php
@@ -3,11 +3,16 @@
 	>
 		<div class="flex justify-between p-4">
 			<div class="text-[2rem] leading-[3rem] tracking-tight font-bold text-black dark:text-white">
-				<a href="">LOGO</a>
+				<a href="/">i-Trampo</a>
 			</div>
 			<div class="flex items-center space-x-4 text-lg font-semibold tracking-tight">
-				<button class="px-6 py-2 text-white transition duration-700 ease-out bg-white border border-black rounded-lg hover:bg-black hover:border hover:text-white dark:border-white dark:bg-inherit dark:text-white dark:hover:bg-white dark:hover:text-indigo-800">Entrar</button>
-				<button class="px-6 py-2 text-indigo-800 transition duration-500 ease-out bg-slate-100 rounded-lg hover:bg-slate-200 hover:ease-in">Cadastre-se</button>
+				@auth    
+                <a class="px-6 py-2 text-indigo-800 transition duration-500 ease-out bg-slate-100 rounded-lg hover:bg-slate-200 hover:ease-in" href="{{ route('dashboard') }}">Dashboard</a>   
+                @endauth
+                @guest
+                <a class="px-6 py-2 text-white transition duration-700 ease-out bg-white border border-black rounded-lg hover:bg-black hover:border hover:text-white dark:border-white dark:bg-inherit dark:text-white dark:hover:bg-white dark:hover:text-indigo-800" href="{{ route('login') }}">Entrar</a>
+				<a class="px-6 py-2 text-indigo-800 transition duration-500 ease-out bg-slate-100 rounded-lg hover:bg-slate-200 hover:ease-in" href="{{ route('register') }}">Cadastre-se</a>
+                @endguest
 			</div>
 		</div>
         
