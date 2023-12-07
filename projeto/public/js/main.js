735fce235f2d3d46784b9f9c28a4c6fe21e7f04f
pedro@gmail.com <pedro@gmail.com>
2023-12-07 12:34:55 -0300

    Upgrade

--- projeto/public/js/main.js
+++ projeto/public/js/main.js
@@ -27,31 +27,67 @@ $(function () {
     });
     });
 
-    $(document).ready(function(){
-        $("#dropdown").change(function(){
-          var selectedOption = $(this).val();
-          
-          $.ajax({
-            url: '/', // Substitua 'seu_arquivo.php' pelo arquivo que processará a requisição
-            method: 'GET', // Pode ser POST ou GET, dependendo da sua implementação
-            data: { option: selectedOption },
-            success: function(response) {
+//Muda o Form Conforme o tipo de usuário
 
-              if (selectedOption === "1") {
-                $("#candidato").show();
-                $("#empresa").hide();
+$(document).ready(function(){
+    $("#dropdown").change(function(){
+      var selectedOption = $(this).val();
+      
+      $.ajax({
+        url: '/', // Substitua 'seu_arquivo.php' pelo arquivo que processará a requisição
+        method: 'GET', // Pode ser POST ou GET, dependendo da sua implementação
+        data: { option: selectedOption },
+        success: function(response) {
 
-              } else if (selectedOption === "2") {
-                $("#empresa").show();
-                $("#candidato").hide();
-              } else if (selectedOption === "0") {
-                $("#empresa").hide();
-                $("#candidato").hide();
-              }
-            },
-            error: function(xhr, status, error) {
-              console.error(error);
-            }
-        });
-      })
-    })
+          if (selectedOption === "1") {
+            $("#candidato").show();
+            $("#candidatoCampo").show();
+            $("#cpfCampo").show();
+            $("#empresa").hide();
+            $("#empresaCampo").hide();
+            $("#cnpjCampo").hide();
+
+          } else if (selectedOption === "2") {
+            $("#empresa").show();
+            $("#empresaCampo").show();
+            $("#cnpjCampo").show();
+            $("#candidato").hide();
+            $("#candidatoCampo").hide();
+            $("#cpfCampo").hide();
+          } else if (selectedOption === "0") {
+            $("#empresa").hide();
+            $("#empresaCampo").hide();
+            $("#cnpjCampo").hide();
+            $("#candidato").hide();
+            $("#candidatoCampo").hide();
+            $("#cpfCampo").hide();
+          }
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+        }
+    });
+  })
+})
+
+$(document).ready(function() {
+  $('#tagInput').on('keydown', function(event) {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+      event.preventDefault();
+
+      const tagValue = $(this).val().trim();
+      if (tagValue !== '') {
+        // Adicionar a tag dentro do campo de input
+        $('#tagInputWrapper').append(`<span class="bg-gray-200 px-2 py-1 rounded-md mr-2 mb-2">${tagValue}<button type="button" class="ml-2 text-gray-700 font-bold">x</button></span>`);
+
+        // Limpar o input
+        $(this).val('');
+      }
+    }
+  });
+
+  // Remover a tag quando o botão "x" é clicado
+  $('#tagInputWrapper').on('click', 'button', function() {
+    $(this).parent().remove();
+  });
+});
--- projeto/resources/views/cadastro_user.blade.php
+++ projeto/resources/views/cadastro_user.blade.php
@@ -17,7 +17,7 @@
   
   @include('components.app.header')
 
-<form class="inset-x-0 mt-20 z-10 w-full px-8 sm:px-16 lg:px-40 py-1 ">
+<form class="inset-x-0 mt-20 z-10 w-full px-8 sm:px-16 lg:px-40 py-1 " id="myForm">
   <!-- component -->
   <div class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 bg-gray-100 ">
     <h2 class="text-xl">Informações do Usuário</h1>
@@ -26,7 +26,7 @@
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-state">
           Tipo de Conta
         </label>
-        <div class="relative">
+        <div class="flex flex-row-reverse items-center">
           <select class="block appearance-none w-full bg-gray-300 border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="dropdown">
             <option value="0">Selecione o tipo de conta</option>
             <option value="1">Candidato</option>
@@ -37,11 +37,36 @@
           </div>
         </div>
       </div>
-      <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+
+      <div class="md:w-1/2 px-3 mb-6 md:mb-0 hidden" id="candidatoCampo">
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
           Nome Completo
         </label>
         <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Gabigol de Arascaeta Henrique">
+        <p class="text-red text-xs italic">Digite o seu nome completo.</p>
+      </div>
+
+      <div class="md:w-1/2 px-3 mb-6 md:mb-0 hidden" id="empresaCampo">
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+          Nome da Empresa
+        </label>
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Gabigol de Arascaeta Henrique">
+        <p class="text-red text-xs italic">Digite o nome da empresa.</p>
+      </div>
+
+      <div class="md:w-1/2 px-3 mb-6 md:mb-0 hidden" id="cnpjCampo">
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+         CNPJ
+        </label>
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="cnpj" type="text" placeholder="12.345.678/0001-10">
+        <p class="text-red text-xs italic">Please fill out this field.</p>
+      </div>
+
+      <div class="md:w-1/2 px-3 mb-6 md:mb-0 hidden" id="cpfCampo">
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+         CPF
+        </label>
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="cnpj" type="text" placeholder="123.456.789-10">
         <p class="text-red text-xs italic">Please fill out this field.</p>
       </div>
     </div>
@@ -127,8 +152,8 @@
   </div>
 
   <!-- form do candidato -->
-  <div class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 bg-gray-100 hidden" id="candidato">
-    <h2 class="text-xl">Informações do Candidato</h1>
+  {{--<div class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 bg-gray-100 hidden" id="candidato">
+    <h2 class="text-xl">Mais Informações do Candidato</h1>
     <div class="mt-6 -mx-3 md:flex mb-6">
       
       <div class="md:w-1/2 px-3 mb-6 md:mb-0">
@@ -181,44 +206,47 @@
       </div>
       
     </div>
-  </div>
+  </div>--}}
 
   <!--Form da Empresa-->
   <div class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 bg-gray-100 hidden" id="empresa">
     <h2 class="text-xl">Cadastro de Vagas</h1>
     <div class="mt-6 -mx-3 md:flex mb-6">
+
+      <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+          Titulo da Vaga 
+        </label>
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Vagabundo Senior">
+        <p class="text-red text-xs italic"></p>
+      </div>
+
       
       <div class="md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
-          Nome da Empresa
+          Tipo de Candidatura
         </label>
-        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Ifodas">
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="PJ">
         <p class="text-red text-xs italic">Please fill out this field.</p>
       </div>
 
       <div class="md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
-         CNPJ
+          Modelo de Trabalho
         </label>
-        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="cnpj" type="text" placeholder="12.345.678/0001-10">
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Remoto">
         <p class="text-red text-xs italic">Please fill out this field.</p>
       </div>
+
+      
     </div>
-    <div class="-mx-3 md:flex mb-6">
-      <div class="md:w-full px-3">
-        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 " for="grid-password">
-          Titulo da Vaga
-        </label>
-        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-password" type="text" placeholder="Rua longe pra caralho">
-        <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
-      </div>
-    </div>
+    
     <div class="-mx-3 md:flex mb-6">
       <div class="md:w-full px-3">
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-password">
           Área da Vaga
         </label>
-        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-password" type="text" placeholder="Logo ali">
+        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="grid-password" type="text" placeholder="Profissional em fazer nada">
         <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
       </div>
     </div>
@@ -228,10 +256,25 @@
         <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-password">
           Descrição
         </label>
-        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm bg-grey-lighter border-gray-500  rounded border " placeholder="Coloque todas as informações necessárias da vaga"></textarea>
+        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm bg-grey-lighter border-gray-500  rounded border " placeholder="Taca qualquer coisa ai"></textarea>
         <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
       </div>
     </div>
+
+    <div class="-mx-3 md:flex mb-6">
+      <div class="md:w-full px-3" id="">
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 " for="grid-password">
+          Habilidades e Ferramentas
+        </label>
+        <div id="tagInputWrapper" class="flex flex-wrap bg-grey-lighter text-grey-darke">
+          <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-gray-500 rounded py-3 px-4 mb-3" id="tagInput" type="text" placeholder="Habilidades pra encher linguiça">
+        </div>
+        <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
+      </div>
+    </div>
+
+    <input class="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-indigo-800
+    rounded-lg transition duration-200 hover:bg-indigo-600 ease" type="submit" value="Enviar">
   </div>
 </form>
 
