ef5558a6df8455588395700245654a17b47af4c5
Welen1911 <welenalmeida2210@gmail.com>
2023-12-24 12:46:26 -0300

    Feat: update and delete perfil photo

--- projeto/app/Http/Controllers/UserController.php
+++ projeto/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\Cidade;
 use App\Models\Estado;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -76,9 +77,9 @@ class UserController extends Controller
         $user = User::find($id);
 
         $estado = Estado::where('nome', $request->estado)->first();
-        
+
         if (!$estado) {
-        
+
             $estado = Estado::create([
                 'nome' => $request->estado,
             ]);
@@ -88,13 +89,12 @@ class UserController extends Controller
 
 
         if (!$cidade) {
-            
+
             $cidade = Cidade::create([
                 'nome' => $request->cidade,
                 'cep' => $request->cep,
                 'estado_id' => $estado->id,
             ]);
-
         }
 
         $user->endereco()->update([
@@ -110,12 +110,22 @@ class UserController extends Controller
             'fixo' => $request->fixo,
         ]);
 
-        $user->update([
-            'profile_photo_path' => $request->perfil
-            ->storeAs('perfis', now() . ".{$request->perfil->getClientOriginalExtension()}",
-            ) ?? $user->profile_photo_path,
-        ]);
+        if ($request->perfil) {
+            try {
+                if ($user->profile_photo_path) {
+                    unlink("storage/{$user->profile_photo_path}");
+                }
+            } catch (Exception $e) {
+            }
 
+            $user->update([
+                'profile_photo_path' => $request->perfil
+                    ->storeAs(
+                        'perfis',
+                        now() . ".{$request->perfil->getClientOriginalExtension()}",
+                    ),
+            ]);
+        }
         return redirect('/dashboard');
     }
 
@@ -128,6 +138,14 @@ class UserController extends Controller
     public function destroy($id)
     {
         $user = User::find($id);
+
+        try {
+            if ($user->profile_photo_path) {
+                unlink("storage/{$user->profile_photo_path}");
+            }
+        } catch (Exception $e) {
+        }
+
         $user->delete();
 
         return redirect()->route('dashboard');
@@ -164,10 +182,10 @@ class UserController extends Controller
 
 
         $estado = Estado::where('nome', $request->estado)->first();
-        
+
 
         if (!$estado) {
-        
+
             $estado = Estado::create([
                 'nome' => $request->estado,
             ]);
@@ -177,13 +195,12 @@ class UserController extends Controller
 
 
         if (!$cidade) {
-            
+
             $cidade = Cidade::create([
                 'nome' => $request->cidade,
                 'cep' => $request->cep,
                 'estado_id' => $estado->id,
             ]);
-
         }
 
         $endereco = $user->endereco()->create([
